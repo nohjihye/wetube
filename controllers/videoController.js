23e9430bcc6300e1e34c9a35ba460d95872c7d2f
jihye's macbook <mac@macui-MacBookPro.local>
2020-01-24 23:08:51 +0900

    working

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,12 @@
-export const home = (req, res) => res.render("home");
-export const search = (req,res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editvideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home" , {pageTitle : 'Home'});
+
+export const search = (req,res) => {
+    const {query : { term : searchingBy}} = req;
+    res.render("search", {pageTitle : 'search', searchingBy})
+};
+
+export const videos = (req, res) => res.render("videos", {pageTitle : 'video'});
+export const upload = (req, res) => res.render("upload", {pageTitle : 'upload'});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : 'video Detail'});
+export const editVideo = (req, res) => res.render("editvideo", {pageTitle : 'edit video'});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : 'delete video'});
